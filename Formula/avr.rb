fb2b7ee4cd5d0bf75faf778ab24f38ffb0b9c875
Leo Lännenmäki <leo.lannenmaki@avrea.com>
2026-06-17 14:24:46 +0300

    avr 0.1.5

--- Formula/avr.rb
+++ Formula/avr.rb
@@ -1,20 +1,20 @@
 class Avr < Formula
   desc "Avrea command-line client"
   homepage "https://avrea.com/"
-  version "0.1.4"
+  version "0.1.5"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/avrea-com/cli/releases/download/v0.1.4/avr_0.1.4_darwin_arm64.tar.gz"
-      sha256 "1af3e04e983aa5ad278235b191530757544c774df1133d2548df7a4284583533"
+      url "https://github.com/avrea-com/cli/releases/download/v0.1.5/avr_0.1.5_darwin_arm64.tar.gz"
+      sha256 "e277b06a52a4810c89c5808eb6b399bc59a69cdc4042a67773e8d869486c2d00"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/avrea-com/cli/releases/download/v0.1.4/avr_0.1.4_linux_amd64.tar.gz"
-      sha256 "421e41c8ec68310e18d3c14504f3fa26d00cbcb0db6cb37a6e5c5d604cf0c129"
+      url "https://github.com/avrea-com/cli/releases/download/v0.1.5/avr_0.1.5_linux_amd64.tar.gz"
+      sha256 "1ad97e4e54bffb64035c947e1b28f32d9b608aa4a5599b5e06141351a89714e9"
     end
   end
 
